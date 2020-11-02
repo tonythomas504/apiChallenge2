c9ac7c96e2e68ef3744fb0394820ef20cde88288
Tony Thomas <tonythomas504@gmail.com>
2020-11-02 08:16:39 -0500

    final changes

--- public/index.js
+++ public/index.js
@@ -9,25 +9,94 @@
 //     .then(data => {
 //         console.log(data);
 //     })
-const key = '&api_key=dXaNGdPm1U5hj6e7CTbGEhd0FqUFMHmR'
-const url = 'http://api.giphy.com/v1/gifs/search?q=lebron+james&api_key=dXaNGdPm1U5hj6e7CTbGEhd0FqUFMHmR&limit=30'
-const giphyApiUrl = url 
 
-fetch(giphyApiUrl)
-    .then(function(res){
+// I broke this up so I can use one variable to fetch
+// const key = 'dXaNGdPm1U5hj6e7CTbGEhd0FqUFMHmR'
+// const searchTerm = 'dogs'
+// const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${searchTerm}`
+
+
+const gifForm = document.getElementById('gif-form')
+const gifInput = document.getElementById('gif-search')
+const resultsElement = document.getElementById('results')
+
+gifForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    const searchTerm = gifInput.value
+    gifSearch(searchTerm)
+})
+//! Wrap fetch in a function and pass through query
+//! You need to put fetch in a function so the fetch doesn't automatically pop up first
+function gifSearch(searchTerm)
+{
+//!Defining the api key and url
+    const key = 'dXaNGdPm1U5hj6e7CTbGEhd0FqUFMHmR'
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${searchTerm}`
+ 
+//!fetching the url and return json results
+    fetch(url)
+        .then(function(res) {
         return res.json()
-    }).then(function(json){
-        console.log(json.data[0].images.fixed_width.url) 
-        let results = document.getElementById('gif-images')
-        let resultsText = ''
-
-
-        json.data.forEach(function(image){
-            console.log(image.images.fixed_width.url);
-            
+        }) .then(function(json){
+//! JSON data is an arrary, so you have to call on a item in the array call the items you want
+//! Fixed width so all the images are the same width
+        console.log(json.data[0].images.fixed_width.url);
+        let resultsHTML = ''
+//!for each function and create variables to tabulate images in window
+    json.data.forEach(function(object){
+        console.log(object)
+        const apiUrl = object.images.fixed_width.url
+        const apiWidth = object.images.fixed_width.width
+        const apiHeight = object.images.fixed_width.height
+        const apiTitle = object.title
+//! appending to image tag from variable to display the images. 
+        resultsHTML+= `<img 
+            src="${apiUrl}" 
+            width="${apiWidth}" 
+            height="${apiHeight}"
+            alt="${apiTitle}"
+            >`
         })
 
-        results.innerHTML = resultsText
-        
+    resultsElement.innerHTML = resultsHTML
+        }) .catch(function(err){
+        console.log(err.message);
+        })
+
+}
+
+
+
+
+
+
+//fetching giphy api with the giphy api variable
+// fetch(giphyApiUrl)
+//     .then(function(res){
+//         //I want to return the json data here so I can see what it looks like
+//         return res.json()
+//     }).then(function(json){
+//         //I console logged here so I select the images
+//         //.fixed_width so all of the images will look semi uniform on the page
+//         console.log(json.data[0].images.fixed_width.url) 
+//         //wanting to have the results equal to the element so I can display the images
+//         let results = document.getElementById('gif-images')
+//         //left this blank so I can set other variables equal to it to display images on page
+//         let resultsText = ''
+
+
+//         json.data.forEach(function(gif){
+//             console.log(gif.images.fixed_width);
+
+//             const url = gif.images.fixed_width.url
+//             const width = gif.images.fixed_width.width
+//             const height = gif.images.fixed_width.height
+//             const title = gif.title
+//             resultsText = `<img src="${url}" width="${width}" height="${height}" alt="${title}">`
+            
+//         })
+
+//         results.innerHTML = resultsText
+
   
-    })
+//     })
